ce0ddad1039b2df850da1e7bcadf33d01320586a
Mahdi Eslami <eslami.mahdi.98@gmail.com>
2024-02-27 17:39:40 +0330

    add font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        "vazir": ["Vazirmatn", ...defaultTheme.fontFamily.sans],
+        vazir: ["Vazirmatn", ...defaultTheme.fontFamily.sans],
       },
     },
   },
