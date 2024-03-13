d1b7202d636ab79b0fd4906408b7d4a9768dc313
Mahdi Eslami <eslami.mahdi.98@gmail.com>
2024-03-13 12:09:58 +0330

    update: make login form to a component

---
+++ components/deleteModal/index.tsx
@@ -0,0 +1,35 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+import React from "react";
+
+const DeleteModal = () => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>Open</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>آیا مطمئن هستید؟</AlertDialogTitle>
+          <AlertDialogDescription>
+            این عملیات غیرقابل بازگشت است
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>انصراف</AlertDialogCancel>
+          <AlertDialogAction>حذف</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default DeleteModal;
