61349316e79f089b70473ecc643c2c452b7d6821
Mahdi Eslami <eslami.mahdi.98@gmail.com>
2024-04-14 10:27:41 +0330

    fix: revert last commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  distDir: "dist",
-  images: {
-    unoptimized: true,
-  },
   env: {
     BASE_URL: "http://localhost:8000/api",
     PRODUCT_IMG: "http://localhost:8000/images/products/images/",
